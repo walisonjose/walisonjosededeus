74b86f20e200a93dc059e2a32688ed54d1492357
walisonjosededeus <walison.josededeus@gmail.com>
2025-02-18 10:31:11 -0300

    UPDATE: Change avatar image and update job title in Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://media.licdn.com/dms/image/v2/D4D03AQEVmpZpyhGojg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1729161753903?e=1735171200&v=beta&t=g09obBFgw7Lz2pup7mcaWKclrMxW8zQBjrp12BmMR3E" alt="Avatar" />
+          <img src="https://avatars.githubusercontent.com/u/11925691?s=400&u=485d09278292ea5abab71f7b838da6ae731b17b3&v=4" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
@@ -17,7 +17,7 @@ function Main() {
             <a href="https://www.linkedin.com/in/walisonjosededeus/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Walison J. Deus</h1>
-          <p>Desenvolvedor Full Stack</p>
+          <p>Desenvolvedor React & React Native</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/walisonjose" target="_blank" rel="noreferrer"><GitHubIcon/></a>
